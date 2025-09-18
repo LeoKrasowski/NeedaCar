84a28fa30063a03ad4031ed20d72a44489bc713d
LeoKrasowski <leokrasowski@gmail.com>
2025-09-18 08:04:00 +0000

    empty

--- src/main/java/com/example/NeedaCar/service/RentalService.java
+++ src/main/java/com/example/NeedaCar/service/RentalService.java
@@ -1,11 +1,5 @@
 package com.example.NeedaCar.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.example.NeedaCar.model.Car;
-import com.example.NeedaCar.model.Reservation;
-
 public class RentalService {
     /*
     This class controls the entire reservation system:
@@ -14,27 +8,4 @@ public class RentalService {
     -Methods for creating bookings: When the client books the car, this class creates booking and updates the status of a car.
     -The history of the reservations: stores a list of all completed bookings.
     */
-    private List<Car> availableCars;
-    private List<Reservation> reservations;
-
-    public RentalService() {
-        availableCars = new ArrayList<>();
-        reservations = new ArrayList<>();
-    }
-
-    public List<Car> getAvailableCars() {
-        return availableCars;
-    }
-
-    public List<Reservation> getReservations() {
-        return reservations;
-    }
-
-    public void addCar(Car car) {
-        availableCars.add(car);
-    }
-
-    public void removeCar(Car car) {
-        availableCars.remove(car);
-    }
 }
